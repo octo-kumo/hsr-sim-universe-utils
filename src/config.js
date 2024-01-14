a28cf228caeed928159fd3313cc6997aaf289a9d
kumo <h1710169@nushigh.edu.sg>
2024-01-14 14:10:27 -0500

    actions

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export const _hsr_root = join(__dirname, '..', 'StarRailData');
 export const json = P => JSON.parse(readFileSync(P).toString());
-export const lang = 'EN';
+export const lang = 'CHS';
